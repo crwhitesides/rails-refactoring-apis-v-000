4293815e9115e97183aef02961df265b2bc7e328
Christian Whitesides <whitesidescr@gmail.com>
2016-11-15 15:27:00 -0700

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -3,7 +3,6 @@ class RepositoriesController < ApplicationController
   def index
     github = GithubService.new({"access_token" => session[:token]})
     @repos_array = github.get_repos
-    binding.pry
   end
 
   def create
