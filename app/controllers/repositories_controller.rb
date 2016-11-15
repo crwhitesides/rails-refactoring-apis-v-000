a0ba546bc3317ed9d2fb5afabdaf34e1105f5f53
Christian Whitesides <whitesidescr@gmail.com>
2016-11-15 15:15:24 -0700

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,12 @@
+require 'pry'
 class RepositoriesController < ApplicationController
   def index
-    response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
-    @repos_array = JSON.parse(response.body)
+    github = GithubService.new
+    # @repos_array = github.get_repos
+    binding.pry
+
+    # response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    # @repos_array = JSON.parse(response.body)
   end
 
   def create
